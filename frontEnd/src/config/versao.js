2ec05e780fc3a4088cd13a03a27373ce2f5f6c6f
Joao Pedro <joao.pedro@up380.com.br>
2026-01-27 12:46:00 -0300

    uma porrada de melhorias como os comentarios

--- frontEnd/src/config/versao.js
+++ frontEnd/src/config/versao.js
@@ -1,6 +1,6 @@
 // === CONFIGURAÇÃO DE VERSÃO DO SISTEMA ===
 // A versão é lida automaticamente do package.json
-import packageJson from '../../../package.json';
+import packageJson from '../../package.json';
 
 export const VERSAO_SISTEMA = packageJson.version;
 
